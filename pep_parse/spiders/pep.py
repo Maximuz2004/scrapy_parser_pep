c1994996f5094677ff65180e2b1d5f9000d80ab4
Максим Титов <matitov2004@gmail.com>
2023-08-25 02:52:18 +0300

    almost ready

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -6,29 +6,29 @@ from pep_parse.items import PepParseItem
 class PepSpider(scrapy.Spider):
     name = "pep"
     allowed_domains = ["peps.python.org"]
-    start_urls = ["https://peps.python.org"]
+    start_urls = ["https://peps.python.org/"]
 
     def parse(self, response):
-        all_peps = response.css('#numerical-index tbody tr')
-        for pep in all_peps:
-            url = pep.css('a.pep.reference.internal::attr(href)').get()
-            pep_number = pep.css('a.pep.reference.internal::text').get()
+        for pep in response.css('#numerical-index tbody tr'):
             yield response.follow(
-                url,
+                pep.css('a.pep.reference.internal::attr(href)').get(),
                 callback=self.parse_pep,
-                cb_kwargs={'pep_number': pep_number}
+                cb_kwargs={
+                    'pep_number': pep.css(
+                        'a.pep.reference.internal::text'
+                    ).get()
+                }
             )
 
-
-
     def parse_pep(self, response, pep_number):
-
-        data = {
-            'number': pep_number,
-            'name': response.css('.page-title::text').get().split(' – ')[1],
-            'status': response.css(
-                'dt:contains("Status") + dd abbr::text'
-            ).get(),
-        }
-
-        yield PepParseItem(data)
+        yield PepParseItem(
+            {
+                'number': pep_number,
+                'name': response.css(
+                    '.page-title::text'
+                ).get().split(' – ')[1],
+                'status': response.css(
+                    'dt:contains("Status") + dd abbr::text'
+                ).get(),
+            }
+        )
